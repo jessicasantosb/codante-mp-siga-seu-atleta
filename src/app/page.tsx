71213b53db82e3e441f2b4577639a9388ce10650
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-07 10:06:37 -0300

    feat: setup prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import db from '@/prisma/db';
+
+export default async function Home() {
+  const sports = await db.sport.findMany();  
+
   return (
     <>
       <h1>Siga seu atleta!</h1>
+      {sports.map((sport) => (
+        <h4 key={sport.id}>{sport.name}</h4>
+      ))}
     </>
   );
 }
