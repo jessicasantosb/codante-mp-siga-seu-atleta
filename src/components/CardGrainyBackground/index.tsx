938f0743cfe6840103d813e13d0810eda05bcc09
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-08 13:53:37 -0300

    fix: suppress hydration warning

--- src/components/CardGrainyBackground/index.tsx
+++ src/components/CardGrainyBackground/index.tsx
@@ -6,6 +6,7 @@ export function CardGrainyBackground() {
     <div
       className='absolute inset-0 border-2 border-dashed border-black'
       style={{ background: getRandomHexColor() }}
+      suppressHydrationWarning
     >
       <div className='h-full w-full card' />
     </div>
