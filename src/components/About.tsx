ac8e125820dcdd69c6f8ab33a1d1190510a32957
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-08 09:01:06 -0300

    fix: change dialog description tag

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,32 +19,34 @@ export function About() {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Sobre o Projeto</DialogTitle>
-          <DialogDescription>
-            <p>
-              Esse é um projeto criado com base no Mini Projeto{' '}
-              <Link
-                href='https://codante.io/mini-projetos/siga-seu-atleta'
-                target='_blank'
-                className='text-yellow-500 font-bold hover:underline'
-              >
-                Siga seu atleta
-              </Link>{' '}
-              da plataforma{' '}
-              <Link
-                href='https://codante.io'
-                target='_blank'
-                className='text-yellow-500 font-bold hover:underline'
-              >
-                Codante
-              </Link>{' '}
-              com o objetivo de aprender mais sobre o NextJS, Shadcn UI, Prisma
-              e SQLite.
-            </p>
-            <br />
-            <p>
-              Com essa aplicação conseguimos ajudar a dar mais visibiliade para
-              os atletas olímpicos e paralímpicos brasileiros.
-            </p>
+          <DialogDescription asChild>
+            <div>
+              <p>
+                Esse é um projeto criado com base no Mini Projeto{' '}
+                <Link
+                  href='https://codante.io/mini-projetos/siga-seu-atleta'
+                  target='_blank'
+                  className='text-yellow-500 font-bold hover:underline'
+                >
+                  Siga seu atleta
+                </Link>{' '}
+                da plataforma{' '}
+                <Link
+                  href='https://codante.io'
+                  target='_blank'
+                  className='text-yellow-500 font-bold hover:underline'
+                >
+                  Codante
+                </Link>{' '}
+                com o objetivo de aprender mais sobre o NextJS, Shadcn UI,
+                Prisma e SQLite.
+              </p>
+              <br />
+              <p>
+                Com essa aplicação conseguimos ajudar a dar mais visibiliade
+                para os atletas olímpicos e paralímpicos brasileiros.
+              </p>
+            </div>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
