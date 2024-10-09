f34f7a401b7bec9246915bba934e29354eb56507
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-09 09:15:59 -0300

    feat: get searched value

--- src/app/_components/Filters/index.tsx
+++ src/app/_components/Filters/index.tsx
@@ -1,6 +1,30 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import React from 'react';
+
 import { SearchInput } from '@/components/ui';
 
 export function Filters() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const q = searchParams.get('q') || '';
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const params = new URLSearchParams(searchParams);
+    const searchedValue = event.target.value;
+
+    if (searchedValue) {
+      params.set('q', searchedValue);
+    } else {
+      params.delete('q');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className='relative flex flex-row md:flex-col lg:flex-row gap-8'>
       <div>
@@ -8,7 +32,9 @@ export function Filters() {
           className='w-56 md:w-full lg-w-56'
           type='text'
           name='q'
-          placeholder='pesquisar'
+          placeholder='Pesquisar'
+          defaultValue={q}
+          onChange={handleSearch}
         />
       </div>
     </div>
