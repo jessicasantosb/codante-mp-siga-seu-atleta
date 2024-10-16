eb4ba114bab14cf65e84cc517667538277af29dc
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-16 20:33:37 -0300

    fix: no-empty-object-type

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,8 +3,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 import { GoSearch } from 'react-icons/go';
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
